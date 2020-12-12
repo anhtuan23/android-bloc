cba294aac65ae6cbdd6e6a3472bc25d0553a20e2
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-12-12 16:07:35 +0700

    inital commit

---
+++ app/src/main/java/com/a10adevelopment/kotlinbloc/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.a10adevelopment.kotlinbloc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import com.a10adevelopment.kotlinbloc.ui.KotlinBlocTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinBlocTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KotlinBlocTheme {
+        Greeting("Android")
+    }
+}
