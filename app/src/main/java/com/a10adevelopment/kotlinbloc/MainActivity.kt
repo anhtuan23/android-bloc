cd8326e64be647c3cbab4012f0e7e5abde58c422
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-12-13 09:05:36 +0700

    improve example app layout

--- app/src/main/java/com/a10adevelopment/kotlinbloc/MainActivity.kt
+++ app/src/main/java/com/a10adevelopment/kotlinbloc/MainActivity.kt
@@ -3,13 +3,18 @@ package com.a10adevelopment.kotlinbloc
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Remove
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.a10adevelopment.kotlinbloc.bloc.CounterEvent
 import com.a10adevelopment.kotlinbloc.bloc.CounterState
 import com.a10adevelopment.kotlinbloc.ui.KotlinBlocTheme
@@ -41,17 +46,33 @@ fun HomeScreen(viewModel: MainViewModel) {
     val counterBloc = viewModel.counterBloc
     val counterState = counterBloc.stateFlow.collectAsState().value
     Scaffold(floatingActionButton = {
-        FloatingActionButton(
-            onClick = {
-                counterBloc.add(CounterEvent.Increase)
+        Column {
+            FloatingActionButton(
+                onClick = {
+                    counterBloc.add(CounterEvent.Increase)
+                }
+            ) {
+                Icon(Icons.Outlined.Add)
             }
-        ) {
-            Icon(Icons.Outlined.Add)
+            Spacer(modifier = Modifier.height(8.dp))
+            FloatingActionButton(
+                onClick = {
+                    counterBloc.add(CounterEvent.Decrease)
+                }
+            ) {
+                Icon(Icons.Outlined.Remove)
+            }
         }
     }) {
-        when (counterState) {
-            is CounterState.Initial -> Text("Initial")
-            is CounterState.Counting -> Text(counterState.count.toString())
+        Column(
+            Modifier.fillMaxWidth().fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            when (counterState) {
+                is CounterState.Initial -> Text("Initial")
+                is CounterState.Counting -> Text(counterState.count.toString())
+            }
         }
     }
 }
